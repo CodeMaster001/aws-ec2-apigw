4f6e92543ece0934c0a753bc1749d814ea9907a0
Your Name <you@example.com>
2022-05-28 12:28:30 +0530

    Updated files

--- ec2-report/modules/pipeline/stacks/pipeline_stack.py
+++ ec2-report/modules/pipeline/stacks/pipeline_stack.py
@@ -12,14 +12,14 @@ from modules.pipeline.stages.api_gw_stage import ApiLambdaStage
 class PipelineStack(Stack):
     def __init__(self, scope: Construct, id: str,  **kwargs):
         super().__init__(scope, id, **kwargs)
-        connection_obj = pipelines.CodePipelineSource.connection("CodeMaster001/aws-test", "main",connection_arn=self.node.try_get_context('codestar_connection'))
+        connection_obj = pipelines.CodePipelineSource.connection("CodeMaster001/aws-test", "pipeline_dev",connection_arn=self.node.try_get_context('codestar_connection'))
         pipeline = pipelines.CodePipeline(self, "Pipeline",
                                           synth=pipelines.ShellStep("Synth",
                                                                     # Use a connection created using the AWS console to authenticate to GitHub
                                                                     # Other sources are available.
                                                                     input=connection_obj,
-                                                                    commands=["npm install -g aws-cdk@2.20.0",
-                                                                              "pip install -r ec2-report/requirements.txt","ls", "pytest -v ec2-report/tests", "cd ec2-reports", "cdk synth"]
+                                                                    commands=["npm install -g aws-cdk@2.20.0","pwd",
+                                                                              "pip install -r ec2-report/requirements.txt","pwd", "pytest -v ec2-report/tests", "cd ec2-reports", "cdk synth"]
 
                                                                     )
                                           )
